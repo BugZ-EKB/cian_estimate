053590f7ac1d26a68d522f24eb9c774aa1ede16b
Egor Shorokhov <egorka87@gmail.com>
2023-10-21 17:26:26 +0400

    UPGRADE ПОправлен код, добавлены спарсенные данные

--- cian_app/config.py
+++ cian_app/config.py
@@ -6,5 +6,4 @@ print(os.path.join(basedir, '..', 'cian_app.db'))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'cian_app.db')
 
-
 SECRET_KEY = 'fansof1091g3sdgds32t32'
---
+++ get_all_flats.py
@@ -0,0 +1,8 @@
+from cian_app import create_app
+
+from cian_app.loader import read_csv, save_flats
+
+app = create_app()
+with app.app_context():
+    cian_data = read_csv('cian_parsing.csv')
+    save_flats(cian_data)
