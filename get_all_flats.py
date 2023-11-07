cc1ef5554496d3de58be09fb2a06fef46e7b6aff
bugz-ekb <117862503+BugZ-EKB@users.noreply.github.com>
2023-11-07 22:37:53 +0400

    Feature/creating flat (#5)
    * REF. Rebase onto main
    
    * ADD. Rebase onto main (2)
    
    * ADD. Rebase onto main(3)
    
    * ADD. добавление новой квартиры в список
    
    ---------
    
    Co-authored-by: Egor Shorokhov <egorka87@gmail.com>

--- get_all_flats.py
+++ get_all_flats.py
@@ -4,5 +4,5 @@ from cian_app.loader import read_csv, save_flats
 
 app = create_app()
 with app.app_context():
-    cian_data = read_csv('cian_parsing.csv')
+    cian_data = read_csv('cian_parsing_full.csv')
     save_flats(cian_data)
