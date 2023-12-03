79d6f2b250e76b17e5a6d4022f548a01d3a3ece0
Egor Shorokhov <egorka87@gmail.com>
2023-12-03 17:31:32 +0400

    FIX. Selectfield for districts

--- cian_app/forms.py
+++ cian_app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import IntegerField, SelectField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
@@ -7,25 +7,35 @@ class FlatForm(FlaskForm):
     rooms_count = IntegerField('Количество комнат',
                                validators=[DataRequired()], render_kw={'class': 'form-control'})
     total_meters = IntegerField('Общая площадь',
-                               validators=[DataRequired()], render_kw={'class': 'form-control'})
+                                validators=[DataRequired()], render_kw={'class': 'form-control'})
     price = IntegerField('Цена квартиры',
-                               validators=[DataRequired()], render_kw={'class': 'form-control'})
+                         validators=[DataRequired()], render_kw={'class': 'form-control'})
     living_meters = IntegerField('Жилая площадь',
-                               validators=[DataRequired()], render_kw={'class': 'form-control'})
+                                 validators=[DataRequired()], render_kw={'class': 'form-control'})
     kitchen_meters = IntegerField('Площадь кухни',
-                               validators=[DataRequired()], render_kw={'class': 'form-control'})
+                                  validators=[DataRequired()], render_kw={'class': 'form-control'})
     floor = IntegerField('Этаж',
-                               validators=[DataRequired()], render_kw={'class': 'form-control'})
+                         validators=[DataRequired()], render_kw={'class': 'form-control'})
     floors_count = IntegerField('Всего этажей в здании',
-                               validators=[DataRequired()], render_kw={'class': 'form-control'})
+                                validators=[DataRequired()], render_kw={'class': 'form-control'})
     year_of_construction = IntegerField('Год постройки',
-                               validators=[DataRequired()], render_kw={'class': 'form-control'})
-    district = StringField('Район',
-                               validators=[DataRequired()], render_kw={'class': 'form-control'})
+                                        validators=[DataRequired()], render_kw={'class': 'form-control'})
+    district = SelectField('Район',
+                           choices=[("Автовокзал", "Автовокзал"),
+                                    ("Верх-Исетский", "Верх-Исетский"),
+                                    ("Дзержинский", "Дзержинский"),
+                                    ("Железнодорожный", "Железнодорожный"),
+                                    ("Кировский", "Кировский"),
+                                    ("Ленинский", "Ленинский"),
+                                    ("Октябрьский", "Октябрьский"),
+                                    ("Орджоникидзевский", "Орджоникидзевский"),
+                                    ("Тагилстроевский", "Тагилстроевский"),
+                                    ("Центр", "Центр"),
+                                    ("Чкаловский", "Чкаловский"),
+                                    ],
+                           validators=[DataRequired()], render_kw={'class': 'form-control'})
     submit = SubmitField('Отправить', render_kw={'class': 'btn btn-success'})
 
 
 class EstimateForm(FlatForm):
-    district = StringField('Район',
-                               validators=[DataRequired()], render_kw={'class': 'form-control'})
     submit = SubmitField('Рассчитать цену', render_kw={'class': 'btn btn-success'})
